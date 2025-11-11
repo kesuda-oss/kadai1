0dbc45ecfabc5eca22acfb6a59e03dde718dab6e
suda <ke.suda@shimade.ac.com>
2025-11-11 16:41:21 +0900

    課題2

--- kadai2.cpp
+++ kadai2.cpp
@@ -1,6 +1,14 @@
-#include "kadai2.h"
+#include <stdio.h>
+double kadai2(double sin , double tai ) {
 
-double bmi(double height_m, double weight) {
-    // TODO: 実装
-    return 0.0;
+
+    return tai*tai/sin;
+}
+int main(void) {
+    int sin , tai;
+    printf("あなたの身長を入力してください");
+    scanf("%f", &sin);
+    printf("あなたの体重を入力して下い");
+    scanf("%f", &tai);
+    printf("%.2f",kadai2(sin , tai));
 }
