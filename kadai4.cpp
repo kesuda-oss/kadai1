39f2941f0b2685876bac6d82396742edc4c639c9
suda <ke.suda@shimade.ac.com>
2025-11-11 16:43:12 +0900

    課題4

--- kadai4.cpp
+++ kadai4.cpp
@@ -1,5 +1,22 @@
-#include "kadai4.h"
+#include <stdio.h>
 
-void drawTree(int h) {
-    // TODO: 実装
+int main(void) {
+    int n;
+    printf("ツリーの高さを入力してください: \n");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        int stars = 2 * i - 1;
+        int spaces = n - i;
+
+        for (int j = 0; j < spaces; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < stars; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
 }
