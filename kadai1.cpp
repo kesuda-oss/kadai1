0c42561cc7cbf5f8bfe04e2d59cd88acd2cd9776
suda <ke.suda@shimade.ac.com>
2025-11-11 16:30:36 +0900

    課題１

--- kadai1.cpp
+++ kadai1.cpp
@@ -1,13 +1,39 @@
-#include "kadai1.h"
-
-char grade(int score) {
-    if (score > 80) {
+#include <stdio.h>
+char grade(int a){
+    if (a < 0 || a > 100) {
+        printf("エラー");
+    }
+    if (a >= 100) {
         return 'A';
-    } else if (score > 60) {
+    }
+    else if (a >= 80) {
         return 'B';
-    } else if (score > 12) {
+    }
+    else if (a >= 60) {
         return 'C';
     }
-    return 'F';
+    else if (a >= 40) {
+        return 'D';
     }
+    else if (a >= 20) {
+        return 'E';
+    }
+    else {
+        return 'F';
+    }
+}
+int main(void) {
+    int score;
+    printf("点数を入力してください: ");
+    scanf("%d", &score);
+
+    char result = grade(score);
+
+    if (score < 0 || score > 100) {
+        printf("エラー\n");
+    } else {
+        printf("あなたの評価は %c です。\n", result);
+    }
+
+    return 0;
 }
